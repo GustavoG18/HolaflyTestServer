4a6d4a93fc3044adad789846cbd0ed4b55ad8abb
GustavoG18 <gguerrerof98@gmail.com>
2023-10-13 12:39:08 +0100

    set up index.ts to connect with hexagonal architecture

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import { UserRepositoryImp } from "./infrastructure/repositories/UserRepositoriesImp";
+import { UserService } from "./application/services/UserService";
+import { UserController } from "./interfaces/controllers/UserController";
+import { setupRoutes } from "./interfaces/routes";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("Testing");
-});
+app.use(express.json());
+
+const userRepository = new UserRepositoryImp();
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+setupRoutes(app, userController);
+
 app.listen(PORT, () => {
   console.log("Servidor corriendo");
 });

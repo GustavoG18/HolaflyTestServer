adb62a98bd063c27b93eeca477bfc11647461a6b
GustavoG18 <gguerrerof98@gmail.com>
2023-10-13 11:13:30 +0100

    Server initialization

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-const message: string = "Hello, World!";
-console.log(message);
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Testing");
+});
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
+});
